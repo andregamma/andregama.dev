ba8a8159120377a62aff7538a8444218e5f612ac
André Gama <andre_gama@outlook.com.br>
2023-10-14 11:35:01 -0300

    chore: altera favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  icons: [
+    { rel: 'icon', url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>' }
+  ],
+}
 
 export default function RootLayout({
   children,
